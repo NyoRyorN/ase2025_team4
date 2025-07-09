0126e277ba19e01aaa89621be5a6e71d77de148d
Jun Masaki <m255766@hiroshima-u.ac.jp>
2025-07-09 11:23:35 +0900

    文字列生成関数作成

--- wordle_ase.go
+++ wordle_ase.go
@@ -1,16 +1,35 @@
-package main 
+package main
 import (
-	"fmt"
-	"regexp"
+    "fmt"
+    "math/rand"
+    "time"
 )
-
+ 
+const letters = "abcdefghijklmnopqrstuvwxyz"
+ 
+func RandomString(n int) string{
+    if n <= 0{
+        return ""
+    }
+ 
+    rand.Seed(time.Now().UnixNano())
+    b := make([]byte, n)
+ 
+    for i := range b {
+        b[i] = letters[rand.Intn(len(letters))]
+    }
+ 
+    return string(b)
+}
+ 
 func main() {
-	fmt.Println("Welcome to Hit and Blow!")
-	fmt.Println("Please guess a 4-digit number (e.g., 1234):")
-	// var correctNumber int 
-	var userNumber string
-	fmt.Scanf("%s",&userNumber)
-	match,_:=regexp.MatchString(`\d{4}`,userNumber)
-	if match != false && len(userNumber) != 4{}
-	fmt.Println("Congratulations! You've guessed the number correctly!")
+    fmt.Println("Welcome to Hit and Blow!")
+    fmt.Println("Please guess a 4-digit number (e.g., 1234):")
+    // var correctNumber int
+    var userNumber string
+    fmt.Scanf("%s",&userNumber)
+    match,_:=regexp.MatchString(`\d{4}`,userNumber)
+    if match != false && len(userNumber) != 4{}
+	fmt.Println(RandomString(8))
+    fmt.Println("Congratulations! You've guessed the number correctly!")
 }
